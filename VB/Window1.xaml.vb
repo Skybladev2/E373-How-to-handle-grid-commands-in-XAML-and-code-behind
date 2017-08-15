d505afcc3079c0b19cd0a3b126ba3460491df7c0
dxBot <DevExpressExampleBot>
2017-08-15 13:06:26 +0300

    initial commit

---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Collections
+Imports DevExpress.Wpf.Grid
+' ...
+
+Namespace WpfApplication1
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+
+			Dim list As New ArrayList()
+			list.Add(New Point() With {.X = 1, .Y = 11})
+			list.Add(New Point() With {.X = 2, .Y = 12})
+			list.Add(New Point() With {.X = 3, .Y = 13})
+			gridControl1.DataSource = list
+		End Sub
+
+		Private Sub button3_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			gridControl1.ExpandAllGroups()
+		End Sub
+		Private Sub button4_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			gridControl1.CollapseAllGroups()
+		End Sub
+	End Class
+End Namespace
